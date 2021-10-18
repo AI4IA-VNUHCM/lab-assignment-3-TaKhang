d10039d786afbd8068061f640b4a820ed05583cc
ITITIU20226 <91868746+TaKhang@users.noreply.github.com>
2021-10-18 16:56:23 +0700

    Update Exercise1.c

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -15,17 +15,30 @@ Ex:
 void Ex1(int arr[], int n){
 	//Your codes here
 	
-int i,j;
-	int sym=0;
-	for (i=0, j= n - 1; (i<= floor((n-1)/2) && j>= ceil((n-1)/2)); i++, j--) {
-		if (arr[i] != arr[j]) 
-			sym++;
-	}
-	sym ==0;
-	printf("Symmetric");
-	if (sym !=0)
-		break;
-	
+ if(n%2 == 0 || n < 3){
+        printf("\nasymmetric\n");
+    }
+    else {
+        int i = 0;
+        int j = n - 1;
+        int sym = 1;
+        
+        while( i < n/2)
+        {
+            if(arr[i] != arr[j]){
+                sym = 0;
+                break;
+            }
+            i++;
+            j--;
+        }
+
+        if (sym == 0)
+            printf("asymmetric");
+        else
+            printf("symmetric");
+    }
+}
     
 }
 
