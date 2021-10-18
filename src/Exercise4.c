eb9689f59d9c86d194bfb3e0c665fee8ff114f58
ITITIU20226 <91868746+TaKhang@users.noreply.github.com>
2021-10-18 17:36:46 +0700

    Update Exercise4.c

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -15,6 +15,46 @@ ______________________________________
 int Ex4(int arr[], int n){
 	//Your codes here
 	
+	 int result = 0;
+
+    for(int i = 0; i < n - 1; i++){
+        if(arr[i] <= arr[i + 1]){
+            result = 1;
+        }else{
+            result = 0;
+            break;
+        }
+    }
+
+    if (result == 0){
+        for(int i = 0; i < n - 1; i++){
+            if(arr[i] >= arr[i + 1]){
+                result = -1;
+            }else{
+                result = 0;
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+	//testing variable, applying it to your algorithm for auto-evaluating
+	argc--;
+	int testcase[argc],i;
+	for(i=0; i<argc;i++){
+		testcase[i] = atoi(argv[i+1]);
+		// printf("%d ", testcase[i]);
+	}
+	
+	int result = Ex4(testcase, argc);
+    printf("%d", result);
+
+    printf("\n");
+
+	
 }
 
 int main(int argc, char *argv[]) {
